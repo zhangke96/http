b0704bc329df44f3c31b6e9397f9c682f9fe5f49
zhangke <zhangke960808@163.com>
2017-08-27 13:41:58 +0800

    update makefile

--- http_server/RequestParser.h
+++ http_server/RequestParser.h
@@ -14,42 +14,9 @@ class RequestParser
 public:
 	typedef int callback_t(http_parser *);
 	typedef int callbackl_t(http_parser *, const char *, size_t);
-	RequestParser() : finished(false), fvpairs()
-	{
-		num = 0;
-		parser = (http_parser *)malloc(sizeof(http_parser));
-		http_parser_init(parser, HTTP_REQUEST);
-		parser->data = this;  // 用来标示对象
-		// 设置http_parser的回调函数
-		/*
-		settings.on_message_begin = call_message_begin_cb_function.target<callback_t >();
-		settings.on_header_field = call_header_field_cb_function.target<callbackl_t >();
-		settings.on_header_value = call__header_value_cb_function.target<callbackl_t >();
-		settings.on_url = call_request_url_cb_function.target<callbackl_t >();
-		settings.on_status = call_response_status_cb_function.target<callbackl_t >();
-		settings.on_body = call_body_cb_function.target<callbackl_t >();
-		 */
-
-		settings.on_message_begin = call_message_begin_cb;
-		settings.on_header_field = call_header_field_cb;
-		settings.on_header_value = call_header_value_cb;
-		settings.on_url = call_request_url_cb;
-		settings.on_status = call_response_status_cb;
-		settings.on_body = call_body_cb;
-		settings.on_headers_complete = call_headers_complete_cb;
-		settings.on_message_complete = call_message_complete_cb;
-		settings.on_chunk_header = call_chunk_header_cb;
-		settings.on_chunk_complete = call_chunk_complete_cb;
-	}
+	RequestParser();
 	bool isFinished() const { return finished; };
-	size_t parser_execute(const char *buf, size_t recved)     /* recved is the length of the content in buf */
-	{
-		++num;
-		size_t nparsed = http_parser_execute(parser, &settings, buf, recved);
-		if (num >= 5)
-			finished = true;
-		return nparsed;
-	}
+	size_t parser_execute(const char *buf, size_t recved);     /* recved is the length of the content in buf */
 	int getMethod() const /* return http method */
 	{
 		return method;
